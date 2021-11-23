c7ebad5969587fdb4686a0099ad8623be8950557
lukaszplawiak <lukaszplawiakzero1@gmail.com>
2021-11-23 18:50:03 +0100

    ninth commit, okta auth implementation -done

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa2v3w4v9e1cFOj35d7',
+        issuer: 'https://dev-25275632.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
